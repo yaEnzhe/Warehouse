8ae8c9056bb89b53099d6e631109abb33a94d025
Angie <enzhe.yanbekova@mail.ru>
2026-03-11 01:25:14 +0300

    Reapply "Созданы формы авторизации и регистрации, создана база данных(таблица с данными пользователей)"
    This reverts commit a32145108b2631c2c5143edcf5a69e5816b3ac53.

---
+++ WarehouseApp/Forms/LoginForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+using WarehouseApp.Classes;
+using WarehouseApp.Forms;
+
+namespace WarehouseApp
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistration_Click(object sender, EventArgs e)
+        {
+            var registration = new RegistrationForm();
+            registration.ShowDialog();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            using (UserContext db = new UserContext())
+            {
+                foreach(var user in db.Users)
+                {
+                    if (txtLogin.Text == user.Login && txtPassword.Text==user.Password)
+                    {
+                        var mainMenuForm = new MainMenuForm();
+
+                        mainMenuForm.ShowDialog();
+                        break;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Неверно введен логин или пароль");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
