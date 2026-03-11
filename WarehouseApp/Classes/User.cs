55a7ea3dc9b5c8ea6a353551f9f0221d9438c25f
Angie <enzhe.yanbekova@mail.ru>
2026-03-12 01:12:15 +0300

    Добавлены summary

--- WarehouseApp/Classes/User.cs
+++ WarehouseApp/Classes/User.cs
@@ -3,22 +3,60 @@ using System;
 
 namespace WarehouseApp.Classes
 {
+    /// <summary>
+    /// Класс пользователя системы
+    /// </summary>
     public class User
     {
+        /// <summary>
+        /// Уникальный идентификатор пользователя
+        /// </summary>
         public long Id { get; set; }
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// Фамилия пользователя
+        /// </summary>
         public string Surname { get; set; }
+        /// <summary>
+        /// Отчество пользователя (при наличии)
+        /// </summary>
         public string Patronymic { get; set; }
+        /// <summary>
+        /// Логин пользователя
+        /// </summary>
         public string Login { get; set; }
+        /// <summary>
+        /// Пароль пользователя
+        /// </summary>
         public string Password { get; private set; }
+        /// <summary>
+        /// Роль пользователя в системе (администратор либо кладовщик)
+        /// </summary>
         public Roles Role { get; set; }
+        /// <summary>
+        /// Дата регистрации пользователя
+        /// </summary>
         public DateTime DateOfRegistration { get; set; }
 
+        /// <summary>
+        /// Метод для хеширования и установки паролей
+        /// </summary>
+        /// <param name="user"> пользователь </param> 
+        /// <param name="text"> пароль заданный пользователем </param>
         public void SetPassword(User user, string text)
         {
            user.Password = text;
         }
 
+        /// <summary>
+        /// Метод для проверки пароля
+        /// </summary>
+        /// <param name="user"> пользователь </param>
+        /// <param name="text"> пароль указанный пользователем </param>
+        /// <returns></returns>
         public bool CheckPassword(User user, string text)
         {
             return user.Password == text;
--- WarehouseApp/Classes/UserContext.cs
+++ WarehouseApp/Classes/UserContext.cs
@@ -2,14 +2,22 @@
 
 namespace WarehouseApp.Classes
 {
+    /// <summary>
+    /// Класс контекста данных для пользователей
+    /// </summary>
     public class UserContext: DbContext
     {
+        /// <summary>
+        /// конструктор класса контекста данных для пользователей
+        /// </summary>
         public UserContext()
             : base("DbConnection")      //имя будущей строки подключения к базе данных
         {
 
         }
-
-        public DbSet<User> Users { get; set; }  //набор сущностей, хранящихся в базе данных
+        /// <summary>
+        /// Набор сущностей(пользователей), хранящихся в базе данных
+        /// </summary>
+        public DbSet<User> Users { get; set; } 
     }
 }
